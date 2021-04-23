c27c5e7d63db8b06382970918772b7a7da53ad92
JariKoot <jarikoot@gmail.com>
2021-04-23 09:26:49 +0200

    updates les

--- code/exercises.R
+++ code/exercises.R
@@ -6,6 +6,8 @@ library(toolboxr)   #For rotating the axes in R
 library(nlme)       #For running multi-level statistical models
 library(ggsignif)   
 library(tools)
+library(RColorBrewer) #For more colours in a graph
+library(kableExtra)   #For more complex table options
 #Exercise 1.2
 
 #A
@@ -156,3 +158,66 @@ md5sums_all %>% select(filename,value)
 md_5_1 <- tools::md5sum("MD5_exampledata_1.txt")
 md_5_1 %>% enframe()
 getwd()
+
+
+
+cbp1 <- c("#999999", "#E69F00", "#56B4E9", "#009E73",
+          "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+library(palmerpenguins)
+
+mass_hist <- ggplot(data = penguins, aes(x = body_mass_g)) +
+  geom_histogram(aes(fill = species),
+                 alpha = 0.8,
+                 position = "identity") +
+  scale_fill_manual(values = c("darkorange","purple","cyan4")) +
+  theme_minimal() +
+  labs(x = "Body mass (g)",
+       y = "Frequency",
+       title = "Penguin body mass")
+
+mass_hist + scale_fill_manual(values = cbp1)
+mass_hist
+library(RColorBrewer)
+display.brewer.all(colorblindFriendly = TRUE)
+
+#
+data_for_table <- mtcars[1:5, 1:6]
+kbl(data_for_table) %>%
+  kable_styling(bootstrap_options = "striped", 
+                full_width = F, 
+                position = "left")
+data_for_table %>%
+  kbl() %>%
+  kable_classic_2(full_width = F) %>%
+  row_spec(0, angle = -45) %>%
+  column_spec(2, color = spec_color(mtcars$mpg[1:5]),
+              link = "https://www.wikipedia.com") %>%
+  column_spec(5, color = "white",
+              background = spec_color(mtcars$hp[1:5], end = 0.7),
+              popover = paste("am:", mtcars$am[1:5]))
+
+
+#
+dinosaur <- c("Triceratops", "Ankylosaurus", "Spinosaurus", "Tyrannosaurus rex", "Velociraptor")
+description <- c("An herbivore dinosaur with two long horns for protection", "An armoured herbivore dinosaur", "A giant theropod, the largest of all predatory dinosaurs", "A large theropod, that relied on his smell and sight for hunting", "A small, bird-like theropod. With large claws to kill his preys")
+dataframe <- data.frame(dinosaur = dinosaur, description = description)
+dataframe %>% kbl() %>% kable_classic_2(full_width = F) %>% 
+  column_spec(1, color = c("red", "green", "brown", "orange", "blue"))
+
+rating <- c(8, 7, 10, 9, 8)
+dataframe_rating <- dataframe %>% mutate(rating = rating)
+View(dataframe_rating)
+#Make a graph
+rating <- c(8.5, 6.5, 10, 9, 8)
+dataframe_rating <- dataframe %>% mutate(rating = rating)
+
+dataframe_rating %>% ggplot(aes(dinosaur, rating, fill = dinosaur)) +
+  geom_col() +
+  labs(title = "Rating my top 5 dinosaurs on a scale from 1 to 10", 
+       y = " ",
+       x = " ") +
+  theme_classic() +
+  theme(text = element_text(size = 15)) +
+  theme(legend.position = "none")
+           
