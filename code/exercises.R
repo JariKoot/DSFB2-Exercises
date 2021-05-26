333236f2fdbd5befe4df2849bc90c6f4f28f4c7d
JariKoot <jarikoot@gmail.com>
2021-05-26 16:45:52 +0200

    exercise 5.3 and lorem

--- code/exercises.R
+++ code/exercises.R
@@ -221,3 +221,59 @@ dataframe_rating %>% ggplot(aes(dinosaur, rating, fill = dinosaur)) +
   theme(text = element_text(size = 15)) +
   theme(legend.position = "none")
            
+
+
+library(htmlwidgets)
+library(leaflet)
+
+m <- leaflet() %>%
+  addTiles() %>%  # Add default OpenStreetMap map tiles
+  addMarkers(lat=52.084036170323024, lng=5.173630727235891, popup="Here be HL7!") 
+# get coordinates from google maps
+
+m  # Print the map
+
+
+library(threejs)
+library(palmerpenguins)
+z <- penguins$bill_length_mm
+x <- penguins$flipper_length_mm
+y <- penguins$body_mass_g
+N = length(levels(penguins$species)) # we want 3 colours
+scatterplot3js(x,y,z, color=rainbow(N)[penguins$species])
+
+
+
+
+
+# Libraries
+library(dygraphs)
+library(xts) 
+
+# get some data
+# Your time column MUST be a time format or numbers
+rabbitdata <- data.frame(
+  time=seq(from=Sys.Date()-(10*365), to=Sys.Date(), by=21 ), 
+  oorlengte=runif((10*365)/21+1,min = 6, max = 10)
+)
+
+rabbitdata$oorlengte[120:138]<-rabbitdata$oorlengte[120:138]+4
+
+# create xts format because dygraph() only eats xts
+rabbitdata.xts <- xts(x = rabbitdata$oorlengte, order.by = rabbitdata$time)
+
+# plot
+dygraph(rabbitdata.xts) %>% 
+  dySeries(label="average ear length (cm)", color="black") %>%
+  dyShading(from=rabbitdata$time[120], to=rabbitdata$time[138], color="#FFE6E6") %>%
+  dyRangeSelector(dateWindow = c(rabbitdata$time[90], rabbitdata$time[150]))
+
+#
+library(plotly)
+fig1 <- plot_ly(economics, x = ~date, y = ~unemploy)
+fig1 <- fig1 %>% add_lines(name = ~"unemploy")
+fig2 <- plot_ly(economics, x = ~date, y = ~uempmed)
+fig2 <- fig2 %>% add_lines(name = ~"uempmed")
+fig <- cowplot(fig1, fig2)
+
+fig
